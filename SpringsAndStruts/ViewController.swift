5b46a41b1dcd50994113e4cd90aa2bdbefa8ae03
tdimnet <thomas.dimnet@gmail.com>
2018-06-28 16:28:50 +0200

    Initial Commit

---
+++ SpringsAndStruts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpringsAndStruts
+//
+//  Created by Thomas Dimnet on 28/06/2018.
+//  Copyright © 2018 Thomas Dimnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
